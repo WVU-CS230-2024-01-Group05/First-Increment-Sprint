d957d76759ec159ba0cd570e41eeb2ce5c92c7a6
ArjunV8 <abv0006@mix.wvu.edu>
2024-03-05 13:00:47 -0500

    added api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "webapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "webapp52497cf0": {
       "AppClientID": "string",
