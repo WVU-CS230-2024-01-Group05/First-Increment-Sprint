960107f83a1d0fe4d00a22ddfc79defa99588d06
Jimbabwe10 <jfk00006@mix.wvu.edu>
2024-03-22 15:11:27 -0400

    account details update

---
+++ src/pages/accountdetails.jsx
@@ -0,0 +1,14 @@
+import React, {useState, UseEffect, useEffect} from 'react';
+
+const accountdetails =()=>{
+    const [userDetails, setUserDetails]=useState(null)
+    useEffect(()=>{ 
+        const fetchDetails=async()=>{
+            try{
+
+            }catch(err){
+                console.log(err)
+            }
+        }
+    },[]);
+}
