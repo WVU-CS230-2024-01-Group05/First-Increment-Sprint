34f45b37b952f544bb230067db85eb333ad7d428
Jimbabwe10 <jfk00006@mix.wvu.edu>
2024-03-25 15:14:30 -0400

    commit react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import search from "./pages/search";
+import {Link} from 'react-router-dom';
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
 import {
