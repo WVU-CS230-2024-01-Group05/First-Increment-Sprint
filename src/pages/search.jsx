b09d5292aa37ef48a54e320b2a5b96da30eaa5c1
Jimbabwe10 <jfk00006@mix.wvu.edu>
2024-03-25 16:39:03 -0400

    commit post

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TextField, Flex } from '@aws-amplify/ui-react';
+import { Link } from 'react-router-dom';
 
 const Search = ({ onSearchChange }) => {
   return <div>
