d5e9d48dd46733c336bc49be41fb659e8eea3cd0
gribelUS <tiagobreunig1@gmail.com>
2024-03-25 15:40:17 -0400

    Add field

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 
 const Post = () => {
+    function add(){
+        var new_chq_no = parseInt($('#total_chq').val())+1;
+        var new_input="<input type='text' id='new_"+new_chq_no+"'>";
+        $('#new_chq').append(new_input);
+        $('#total_chq').val(new_chq_no)
+      }
+      function remove(){
+        var last_chq_no = $('#total_chq').val();
+        if(last_chq_no>1){
+          $('#new_'+last_chq_no).remove();
+          $('#total_chq').val(last_chq_no-1);
+        }
+      }
+
     return(
         <div className="recipeForm">
             <h1>Create your recipe</h1>
             <input type="text" placeholder="Recipe name" />
             <input type="text" placeholder="Description"/> 
-            
+            <div class="appending_div">
+                <p>Step-by-step instructions:</p>
+            <div className="recipeInstructions">
+                <input type="text">
+                    <button onclick="add()">Add</button>
+                    <button onclick="remove()">remove</button>
+                    <div id="new_chq"></div>
+                    <input type="hidden" value="1" id="total_chq" />
+                </input>
+            </div>
+            </div>
         </div>
     )
 
