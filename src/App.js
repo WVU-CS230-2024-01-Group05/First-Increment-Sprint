e2b39a176234b7fc39a4160586667e7602b566af
IanSchied02 <133838293+IanSchied02@users.noreply.github.com>
2024-03-27 16:10:49 -0400

    commit updated app.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Search from "./pages/search";
 import AccountDetails from "./pages/accountDetails";
+import './App.css';
 import Post from "./pages/Post";
 import {
   BrowserRouter,
@@ -19,134 +20,29 @@ import {
   View,
   withAuthenticator,
 } from "@aws-amplify/ui-react";
-import { listNotes } from "./graphql/queries";
-import {
-  createNote as createNoteMutation,
-  deleteNote as deleteNoteMutation,
-} from "./graphql/mutations";
 import { generateClient} from 'aws-amplify/api';
 import { uploadData, getUrl, remove } from 'aws-amplify/storage';
 
-function App(){
-  return(
+function App() {
+  return (
     <div className="App">
-      <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<Search/>}/>
-          <Route path="/post" element={<Post/>}/>
-          <Route path="/account" element={<AccountDetails/>}/>
-      </Routes>
-      </BrowserRouter>
+      <Router>
+        <div className="App-header">
+          <nav>
+            <Link to="/" className="link-style"><button className="button-account">Search</button></Link>
+            <Link to="/post" className="link-style"><button className="button-post">Post</button></Link>
+            <Link to="/account" className="link-style"><button className="button-signout">Account Details</button></Link>
+            <button className="button-signout" onClick={() => alert('Sign Out Logic Here')}>Sign Out</button>
+          </nav>
+        </div>
+        <Routes>
+          <Route path="/" element={<Search />} />
+          <Route path="/post" element={<Post />} />
+          <Route path="/account" element={<AccountDetails />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
-/*const client = generateClient();
-
-const App = ({ signOut }) => {
-  const [notes, setNotes] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("");
-
-  useEffect(() => {
-    fetchNotes();
-  }, []);
-
-  async function fetchNotes() {
-    const apiData = await client.graphql({ query: listNotes });
-    const notesFromAPI = apiData.data.listNotes.items;
-    await Promise.all(
-      notesFromAPI.map(async (note) => {
-        if (note.image) {
-          const url = await getUrl({ key: note.name});
-          note.image = url.url;
-        }
-        return note;
-      })
-    );
-    setNotes(notesFromAPI);
-  }
-
-  async function createNote(event) {
-    event.preventDefault();
-    const form = new FormData(event.target);
-    const image = form.get("image");
-    const data = {
-      name: form.get("name"),
-      description: form.get("description"),
-      image: image.name,
-    };
-    if (!!data.image) await uploadData({
-      key: data.name,
-      data: image
-    });
-    await client.graphql({
-      query: createNoteMutation,
-      variables: { input: data },
-    });
-    fetchNotes();
-    event.target.reset();
-  }
-
-  async function deleteNote({ id, name }) {
-    const newNotes = notes.filter((note) => note.id !== id);
-    setNotes(newNotes);
-    await remove({ key: name });
-    await client.graphql({
-      query: deleteNoteMutation,
-      variables: { input: { id } },
-    });
-  }
-
-  const filteredNotes = notes.filter(note =>
-    note.name.toLowerCase().includes(searchQuery.toLowerCase())
-  );
-
-  return (
-    <View className="App">
-      <Heading level={1} color="red">Nom Nom Next</Heading>
-      <View as="form" margin="3rem 0" onSubmit={createNote}>
-        <Flex direction="row" justifyContent="center">
-          <TextField
-            name="name"
-            placeholder="Searchbar placeholder"
-            label="Note Name"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <Button>
-            <Link to="/pages/search">Search</Link>
-          </Button>
-        </Flex>
-      </View>
-      <Heading level={2}>"Recipes Placeholder"</Heading>
-      <View margin="3rem 0">
-        {filteredNotes.map((note) => (
-          <Flex
-            key={note.id || note.name}
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Text as="strong" fontWeight={700}>
-              {note.name}
-            </Text>
-            <Text as="span">{note.description}</Text>
-            {note.image && (
-              <Image
-                src={note.image}
-                alt={`visual aid for ${note.name}`}
-                style={{ width: 400 }}
-              />
-            )}
-            <Button variation="link" onClick={() => deleteNote(note)}>
-              Delete note
-            </Button>
-          </Flex>
-        ))}
-      </View>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
-  );
-};*/
 
 export default App;
