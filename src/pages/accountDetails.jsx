7bb6f4d81d58dbbe0cddb0a9b00067860e296983
Jimbabwe10 <jfk00006@mix.wvu.edu>
2024-03-27 15:30:10 -0400

    added user details

--- src/pages/accountDetails.jsx
+++ src/pages/accountDetails.jsx
@@ -3,18 +3,31 @@ import React, {useState, UseEffect, useEffect} from 'react';
 const AccountDetails =()=>{
     const [userDetails, setUserDetails]=useState(null)
     useEffect(()=>{ 
-        const fetchDetails=async()=>{
+        async function fetchDetails() {
             try{
-                
+                const user = await Auth.currentAuthenticatedUser();
+                const attributes = await Auth.userAttributes(user);
+                setUserAttributes(attributes);
             }catch(err){
                 console.log(err)
             }
         }
+        fetchDetails();
     },[]);
 
 return(
     <div>
         <h1>Account Details</h1>
+        {userAttributes && (
+        <ul>
+          {userAttributes.map(attribute => (
+            <li key={attribute.username}>
+              <strong>{attribute.username}: </strong>
+              {attribute.Value}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
 )
 }
