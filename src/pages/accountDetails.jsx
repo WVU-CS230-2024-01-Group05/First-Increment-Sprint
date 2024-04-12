3711165701ceb932831d61eff4317d15e96c5775
IanSchied02 <133838293+IanSchied02@users.noreply.github.com>
2024-04-12 16:09:36 -0400

    commit made account details pretty

--- src/pages/accountDetails.jsx
+++ src/pages/accountDetails.jsx
@@ -1,50 +1,49 @@
-import React, {useState, UseEffect, useEffect} from 'react';
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-    Link} from 'react-router-dom';
-    
-const AccountDetails =()=>{
-    /*const [userAttributes, setUserAttributes]=useState(null)
-    useEffect(()=>{ 
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const AccountDetails = () => {
+    // Uncomment and use the following code if you want to fetch user details
+    /*
+    const [userAttributes, setUserAttributes] = useState(null);
+    useEffect(() => {
         async function fetchDetails() {
-            try{
+            try {
                 const user = await Auth.currentAuthenticatedUser();
                 const attributes = await Auth.userAttributes(user);
                 setUserAttributes(attributes);
-            }catch(err){
-                console.log(err)
+            } catch (err) {
+                console.log(err);
             }
         }
         fetchDetails();
-    },[]);*/
+    }, []);
+    */
 
-return(
-    <div>
+    return (
+        <div className="account-details">
             <h1>Account Details</h1>
             <form>
-                <div>
+                <div className="form-group">
                     <label htmlFor="username">Username:</label>
                     <input type="text" id="username" name="username" />
                 </div>
-                <div>
+                <div className="form-group">
                     <label htmlFor="password">Password:</label>
                     <input type="password" id="password" name="password" />
                 </div>
-                <div>
+                <div className="form-group">
                     <label htmlFor="fullName">Full Name:</label>
                     <input type="text" id="fullName" name="fullName" />
                 </div>
-                <div>
+                <div className="form-group">
                     <label htmlFor="description">Biography:</label>
-                    <textarea id="description" name="description" />
+                    <textarea id="description" name="description"></textarea>
                 </div>
-                <button type="submit">Save Changes</button>
+                <button type="submit" className="save-button">Save Changes</button>
             </form>
-            <Link to="/">Home</Link>
+            <Link to="/" className="home-link">Home</Link>
         </div>
-)
+    );
 }
 
 export default AccountDetails;
