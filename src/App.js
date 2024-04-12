889f7e1287e4e684b7951045c80de2907d9cf4a0
Jimbabwe10 <jfk00006@mix.wvu.edu>
2024-04-12 15:25:50 -0400

    GraphQL

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import {ReactDOM} from "react";
 import {render} from "react-dom";
+import ReactDOM from 'react-dom/client';
 import Search from "./pages/search";
 import AccountDetails from "./pages/accountDetails";
 import './App.css';
