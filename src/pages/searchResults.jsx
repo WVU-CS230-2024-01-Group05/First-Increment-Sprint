34969a09dc4187439a81f03b0ab1af6783690009
ArjunV8 <abv0006@mix.wvu.edu>
2024-04-12 12:24:37 -0400

    Update searchResults.jsx

--- src/pages/searchResults.jsx
+++ src/pages/searchResults.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const searchResults = () => {
+
+
+ return(
     <div className = "App-header">
-        
+
     </div>
-
+  )
 }
-
 export default searchResults;
