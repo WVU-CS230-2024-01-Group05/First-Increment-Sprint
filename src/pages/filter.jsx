441437a085792708a8ed1ef5eb79123429fffc21
ahilleas08 <aif00002@mix.wvu.edu>
2024-04-16 17:52:58 -0400

    filter function

--- src/pages/filter.jsx
+++ src/pages/filter.jsx
@@ -1,14 +1,65 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import testRecipes from "./pages/recipes";
 
 const Filter = () => {
+    const [searchInput, setSearchInput] = useState('');
+    const [ingredients, setIngredients] = useState([]);
+    const [filteredRecipes, setFilteredRecipes] = useState([]);
+  
+    const handleAddIngredient = () => {
+      if (!ingredients.includes(searchInput)) {
+        setIngredients([...ingredients, searchInput.trim()]);
+        setSearchInput('');
+      }
+    };
+  
+    const handleRemoveIngredient = (ingredientToRemove) => {
+      const newList = ingredients.filter(
+        (ingredient) => ingredient !== ingredientToRemove
+      );
+      setIngredients(newList);
+    };
     
+  
+    const handleSearch = () => {
+      const filtered = recipes.filter((recipe) =>
+        recipe.ingredients.some((ingredient) =>
+          ingredients.includes(ingredient.toLowerCase())
+        )
+      );
+      setFilteredRecipes(filtered);
+    };
+  
     return (
-        <div className="filter">
-            <h1>Filter By Ingredients</h1>
-            
+      <div>
+        <div>
+          <input
+            type="text"
+            placeholder="Please enter your ingredients"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+          <button onClick={handleAddIngredient}>Click to Add Ingredient</button>
         </div>
+        <div>
+          {ingredients.map((ingredient) => (
+            <div key={ingredient}>
+              {ingredient}
+              <button onClick={() => handleRemoveIngredient(ingredient)}>X</button>
+            </div>
+          ))}
+        </div>
+        <button onClick={handleSearch}>Search Recipes</button>
+        <ul>
+          {filteredRecipes.map((recipe) => (
+            <li key={recipe.id}>
+              <h3>{recipe.title}</h3>
+              <p>Ingredients: {recipe.ingredients.join(', ')}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     );
-}
+  };
 
 export default Filter;
---
+++ src/pages/testRecipes.js
@@ -0,0 +1,16 @@
+const testRecipes = [
+    {
+      id: 1,
+      title: 'Pizza',
+      ingredients: ['dough', 'cheese', 'sauce', 'pepperoni'],
+    },
+    {
+      id: 2,
+      title: 'BLT',
+      ingredients: ['bacon', 'lettuce', 'tomato', 'bread'],
+    }
+    
+  ];
+
+  export default testRecipes;
+  
