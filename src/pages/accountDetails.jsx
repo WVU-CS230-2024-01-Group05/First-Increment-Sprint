f8bc606b5d1c0cc66682ad5ca8079e57925b9ca8
Jimbabwe10 <jfk00006@mix.wvu.edu>
2024-04-25 13:07:21 -0400

    account details

--- src/pages/accountDetails.jsx
+++ src/pages/accountDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Auth } from '@aws-amplify';
+import { Auth } from '../@aws-amplify';
 
 const AccountDetails = () => {
     const [userAttributes, setUserAttributes] = useState(null);
