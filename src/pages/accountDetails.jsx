2d5a934f49564ceb01c350fa9ebd14b2e17c8e77
Jimbabwe10 <jfk00006@mix.wvu.edu>
2024-04-25 13:54:14 -0400

    Account Details pls work

--- src/pages/accountDetails.jsx
+++ src/pages/accountDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { fetchUserAttributes } from './Account-Details';
+import { fetchUserAttributes } from '..Account-Details';
 
 const AccountDetails = () => {
     const [userAttributes, setUserAttributes] = useState(null);
