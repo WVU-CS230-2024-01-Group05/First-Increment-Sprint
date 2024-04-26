61699825e314df9071a7701bfa929b6b0a70e4e2
Jimbabwe10 <jfk00006@mix.wvu.edu>
2024-04-26 15:08:53 -0400

    Account Details

--- src/Account-Details.js
+++ src/Account-Details.js
@@ -1,8 +1,8 @@
-import {Authenticator,AccountSettings} from "@aws-amplify/ui-react"
+import {useAuthenticator,AccountSettings} from "@aws-amplify/ui-react"
 
     export const fetchUserAttributes = async () => {
         try {
-            const currentUser = await Authenticator.currentAuthenticatedUser();
+            const {currentUser} = useAuthenticator((context)=>[context.user]);
             return currentUser.attributes;
         } catch (error) {
             console.error('Error fetching user attributes:', error);
