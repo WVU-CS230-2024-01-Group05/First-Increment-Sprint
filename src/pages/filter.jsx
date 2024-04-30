9f73b8e90aaf7d08357d61b213fc819af157141c
ahilleas08 <aif00002@mix.wvu.edu>
2024-04-30 16:29:49 -0400

    Update filter.jsx

--- src/pages/filter.jsx
+++ src/pages/filter.jsx
@@ -63,7 +63,7 @@ const Filter = () => {
         <button className="filterButton" onClick={Search}>Search Recipes</button>
         <ul>
         <p> 
-        <h2> Recipes Exclusive to your ingredients: </h2>
+        <h2> Recipes you can make now: </h2>
         {exclusiveRecipes.map((recipe) => (
             <li key={recipe.id}>
               <h3>{recipe.title}</h3>
@@ -73,7 +73,7 @@ const Filter = () => {
           ))}
           </p>
           <p> 
-          <h2> Recipes that include your ingredients: </h2>
+          <h2> Recipes you're missing a few ingredients: </h2>
           {filteredRecipes.map((recipe) => (
             <li key={recipe.id}>
               <h3>{recipe.title}</h3>
