a8353ac88871f2a77da239d571617f040d87fa71
your user name <133838293+IanSchied02@users.noreply.github.com>
2024-04-30 16:47:11 -0400

    commit

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -11,9 +11,29 @@ import {
   withAuthenticator,
 } from "@aws-amplify/ui-react";
 
+function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+      fetchPosts();
+  }, []);
+
+  async function fetchPosts() {
+      try {
+          const response = await fetch('https://z7pmt81mal.execute-api.us-east-2.amazonaws.com/posts');
+          const data = await response.json();
+          setPosts(data);
+      } catch (error) {
+          console.error('Error fetching data:', error);
+      }
+  }
+}
+
 const Search = ({ onSearchChange }) => {
 
   const { signOut } = useAuthenticator();
+
+
 
 
   return (
@@ -47,6 +67,21 @@ const Search = ({ onSearchChange }) => {
             </nav>
           </div>
         </View>
+        <div>
+          <h1>Posts from DynamoDB</h1>
+          <ul>
+            {posts && posts.map(post => (
+              <li key={post.PostID}>
+                <h3>{post.Title}</h3>
+                <p>Description: {post.Description}</p>
+                <p>Ingredients: {post.Ingredients}</p>
+                <p>Directions: {post.Directions}</p>
+                <p>Likes: {post.Likes}</p>
+                <hr />
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
